f4d30fc60f12042d07492055c480fc0312f48caf
HaoG <hao1027723268@hotmail.com>
2023-09-18 18:30:00 -0600

    fisrt change

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio_app'
 ]
+# Add support for authenticating users 
 
+AUTHENTICATION_BACKENDS = [ 
+
+'django.contrib.auth.backends.ModelBackend', 
+
+] 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
