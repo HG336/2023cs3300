c59f2e4cbfa71d72aa7e83a59ac72369fff1c932
HaoG <hao1027723268@hotmail.com>
2023-09-18 22:34:02 -0600

    secound add

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
-# Create your views here.
+# Create your views here. 
+
+def index(request): 
+
+ 
+# Render the HTML template index.html with the data in the context variable. 
+
+   return render( request, 'portfolio_app/index.html') 
+
