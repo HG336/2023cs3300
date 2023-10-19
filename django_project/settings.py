81283119085fc3e8f7bd314726eb2c4acc077b1f
HaoG <hao1027723268@hotmail.com>
2023-10-18 23:05:24 -0600

    1

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_app'
+    'members',
+    'bootstrap5',
 ]
 # Add support for authenticating users 
 
