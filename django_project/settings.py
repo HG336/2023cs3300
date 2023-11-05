61e1b467535cf640efaf03348d4dd3671c7a755c
HaoG <hao1027723268@hotmail.com>
2023-11-04 19:04:59 -0600

    save1

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio_app'
-    'members',
+    'portfolio_app',
     'bootstrap5',
 ]
 # Add support for authenticating users 
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.views import generic
+from .models import Student
 
 # Create your views here. 
 
@@ -10,3 +12,17 @@ def index(request):
 
    return render( request, 'portfolio_app/index.html') 
 
+class StudentListView(generic.ListView): 
+
+   model = Student 
+
+class StudentDetailView(generic.DetailView): 
+
+   model = Student 
+def index(request): 
+
+   student_active_portfolios = Student.objects.select_related('Portfolio').all().filter(Portfolio__is_active=True) 
+
+   print("active portfolio query set", student_active_portfolios) 
+
+   return render( request, 'portfolio_app/index.html', {'student_active_portfolios':student_active_portfolios}) 
